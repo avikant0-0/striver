06cf23e712421e03173fb14e1ffe5a9634df3f26
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2024-06-15 14:00:03 +0530

    Time: 414 ms (40.63%), Space: 127.4 MB (6.42%) - LeetHub

---
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/2162-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -0,0 +1,58 @@
+using ll = long long ;
+class Solution {
+public:
+    
+    vector<vector<ll>> a,b;
+    ll n ;
+    void meetinmiddle1(auto &nums,ll i,ll taken,ll sum){
+        if(i == n) {
+            a[taken].push_back(sum);
+            return ;
+        }
+        meetinmiddle1(nums,i+1,taken+1,sum+nums[i]);
+        meetinmiddle1(nums,i+1,taken,sum);
+    }
+    void meetinmiddle2(auto &nums,ll i,ll taken,ll sum){
+        if(i == 2*n) {
+            b[taken].push_back(sum);
+            return ;
+        }
+        meetinmiddle2(nums,i+1,taken+1,sum+nums[i]);
+        meetinmiddle2(nums,i+1,taken,sum);
+    }
+    ll find_first_false(ll l ,ll r, auto f) {
+        // --l ; ++r;
+        while( r - l > 1 ){
+           ll m = r + (l - r) / 2 ; ;
+            if(f(m)) l = m ;
+            else r = m;
+        } 
+        return l; 
+    }
+
+    int minimumDifference(vector<int>& nums) {
+        n = nums.size() / 2 ;
+        a.assign(n+1,vector<ll>());
+        b.assign(n+1,vector<ll>());
+        meetinmiddle1(nums,0LL,0LL,0LL);
+        meetinmiddle2(nums,n,0LL,0LL);
+      
+        for(auto &j : b) sort(j.begin(),j.end());
+        ll mindiff = LLONG_MAX;
+        ll sum = accumulate(nums.begin(),nums.end(),0LL);
+        for(ll i =0 ; i <= n ; i++) {
+            for(ll j : a[i]) {
+                ll target = sum / 2 - j ;
+                // for(ll k : b[n-i]){
+                //     if(k <= target) mindiff = min(mindiff,abs(sum-2*(k+j)));
+                //     else break;
+                // }
+                ll k = find_first_false(0,b[n-i].size(),[&](ll mid){
+                    return b[n-i][mid] <= target;
+                });
+                if(k >= 0  && k < b[n-i].size() ) mindiff = min(mindiff,abs(sum-2*(b[n-i][k]+j)));
+            }
+        }
+        return mindiff ; 
+    }
+};
