63c089917fe686bacabe78b990fbd68eecbc0af2
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2024-06-15 15:14:02 +0530

    Time: 202 ms (41.07%), Space: 500.2 MB (5.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,23 +1,21 @@
-using ll = long long;
-
+using ll = long long ;
 class Solution {
 public:
-    ll targetsum;
+    ll target ;
     vector<vector<ll>> dp;
-    bool solve(auto &nums, ll i , ll sum ) {
-        if(i == ll(nums.size())) {
-            if(sum == targetsum/2) return true;
-            else return false;
+    ll f(auto &nums,ll i,ll sum){
+        if(i == nums.size()) {
+            if(sum == target) return dp[i][sum] = true;
+            else return dp[i][sum] = false;
         }
-        if(dp[i][sum] != -1 ) return dp[i][sum];
-        if(sum <= targetsum) return dp[i][sum] = solve(nums,i+1,sum) || solve(nums,i+1,sum + nums[i]) ;
-        else return dp[i][sum] = solve(nums,i+1,sum);
+        if(dp[i][sum] != -1) return dp[i][sum];
+        return dp[i][sum] = f(nums,i+1,sum) || f(nums,i+1,sum+nums[i]) ; 
     }
     bool canPartition(vector<int>& nums) {
-    ll n = nums.size();
-    dp.assign(n+1,vector<ll>(20000, -1)) ;
-    targetsum = accumulate(nums.begin(),nums.end(),0) ;
-    if(targetsum % 2 != 0) return false;
-    return solve(nums,0,0) ;    
+        ll sum = accumulate(nums.begin(),nums.end(),0LL);
+        if(sum%2 != 0) return false;
+        dp.assign(nums.size()+1,vector<ll>(2*sum+1,-1));
+        target = sum / 2;
+        return f(nums,0,0);
     }
 };
