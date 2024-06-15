70300148b07929f883ce8e0a5f0793242846adfe
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2024-06-15 14:24:18 +0530

    Time: 26 ms (47.99%), Space: 272.3 MB (5.44%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,24 @@
 using ll = long long ;
+#include<bits/stdc++.h>
+
 class Solution {
 public:
     ll maintarget;
+    vector<vector<ll>> dp;
+    ll offset = 0;
     ll f(auto &nums,ll i , ll sum) {
         if( i == nums.size() ) {
             if(sum == maintarget) return 1;
             else return 0 ;
         }
-        return f(nums,i+1,sum+nums[i]) + f(nums,i+1,sum-nums[i]);
+        if(dp[i][sum+offset] != -1 ) return dp[i][sum+offset];
+        return dp[i][sum+offset] = f(nums,i+1,sum+nums[i]) + f(nums,i+1,sum-nums[i]);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         maintarget = target ;
+        ll sum = accumulate(nums.begin(),nums.end(),0LL);
+        for(int i =0 ; i < nums.size(); i++ ) offset += abs(nums[i]) ; 
+        dp.assign(nums.size()+1,vector<ll>(10000,-1));
         return f(nums,0,0);
     }
 };
