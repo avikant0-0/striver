bcbea1082d2b23b550eecfaeaef15bff0c67d34d
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2024-06-27 21:45:36 +0530

    Time: 21 ms (49.25%), Space: 12.7 MB (38.93%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    void eraseit(string &s) {
-    int n = s.length();
-    for(int i =0 ; i < n - 1 ;i++) {
-        if(s[i] == s[i+1]) {
-            s.erase(s.begin()+i+1);
-            s.erase(s.begin()+i);
-            break;
-        }
-    }
-}
+    
+
     string removeDuplicates(string s) {
-        string temp = s ;
-        while(true){
-            eraseit(s);
-            if(s == temp ) break;
-            temp = s; 
+        stack<char> st;
+        string result ;
+        for(int i = 0 ; i < s.length(); i++){
+            if(st.empty() || s[i]!=st.top()) st.push(s[i]);
+            else st.pop();
+            
         }
-        return s; 
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+        reverse(result.begin(),result.end());
+        return result;
     }
 };
