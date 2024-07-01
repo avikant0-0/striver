43a7c7ca411281a2d072632ac9e4e82b7b2e35e0
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2024-07-01 17:31:33 +0530

    Time: 3 ms (47.32%), Space: 7.4 MB (93.47%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
     int f(auto &s,auto &t,int m,int n){
         if(m < 0 || n < 0) return 0; 
-        if(dp[m][n]!=-1) return dp[m][n];
-        if(s[m]==t[n]) return dp[m][n] = 1 + f(s,t,m-1,n-1);
-        else return dp[m][n] = f(s,t,m,n-1);
+        if(s[m]==t[n]) return  1 + f(s,t,m-1,n-1);
+        else return  f(s,t,m,n-1);
     }
     bool isSubsequence(string s, string t) {
         int m = s.length();
         int n = t.length();
-        dp.assign(m,vector<int>(n,-1));
         if(m > n) return false;
         return f(s,t,m-1,n-1) == m; 
     }
