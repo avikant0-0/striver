f96512cc32d4094d52c36b8580af619630f595b3
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2024-12-09 14:16:16 +0530

    Time: 10 ms (11.51%), Space: 15.2 MB (64.45%) - LeetHub

---
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,84 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        
+        if(k == 0){
+            return {target->val}; 
+        }
+        map<TreeNode*,TreeNode*> par;
+        queue<TreeNode*> q;
+        q.push(root);
+        par[root] = NULL;
+        while(!q.empty())
+        {
+            int size = q.size();
+            for(int i = 0; i < q.size(); i++)
+            {
+                TreeNode* front = q.front();
+                q.pop();
+
+                if(front->left) par[front->left] = front, q.push(front->left);
+                if(front->right) par[front->right] = front, q.push(front->right); 
+            }
+        }
+        for(auto i : par) {
+            if(i.second) cout << i.first->val <<" " << i.second->val << endl;
+        }
+
+        vector<int> ans;
+
+        q.push(target);
+        int dist = 0 ;
+
+        vector<int> visited(501,0);
+
+        while(!q.empty())
+        {   
+
+            int size = q.size() ;
+            ++dist;
+
+            for(int i= 0 ; i < size ;i++)
+            {
+                TreeNode* front = q.front() ;
+                q.pop() ; 
+                visited[front->val] = 1; 
+
+                if(front->left && !visited[front->left->val]) {
+                    if(dist == k) ans.push_back(front->left->val);
+                    else {
+                        q.push(front->left);
+                    }
+                }
+
+                if(front->right && !visited[front->right->val]) {
+                    if(dist == k) ans.push_back(front->right->val);
+                    else {
+                        q.push(front->right);
+                    }
+                }
+
+                if(par[front] != NULL && !visited[par[front]->val]) {
+                    if(dist == k) ans.push_back(par[front]->val);
+                    else {
+                        q.push(par[front]);
+                    }
+                }
+            }
+        }
+
+    
+        return ans; 
+
+    }
+
+};
