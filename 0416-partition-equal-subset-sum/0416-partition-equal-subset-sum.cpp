3bbd99ee2bfb09c68849205251a1d1f0cdcd2d94
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2024-12-12 20:30:50 +0530

    Time: 111 ms (72.11%), Space: 101.1 MB (18.76%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,21 +1,22 @@
 using ll = long long ;
 class Solution {
 public:
-    ll target ;
-    vector<vector<ll>> dp;
-    ll f(auto &nums,ll i,ll sum){
+    int target ;
+    vector<vector<int>> dp;
+    int f(auto &nums,int i,int sum){
         if(i == nums.size()) {
             if(sum == target) return dp[i][sum] = true;
             else return dp[i][sum] = false;
         }
         if(dp[i][sum] != -1) return dp[i][sum];
-        return dp[i][sum] = f(nums,i+1,sum) || f(nums,i+1,sum+nums[i]) ; 
+        if(sum + nums[i] <= target) return dp[i][sum] = f(nums,i+1,sum) || f(nums,i+1,sum+nums[i]) ; 
+        else return dp[i][sum] = f(nums,i+1,sum) ; 
     }
     bool canPartition(vector<int>& nums) {
-        ll sum = accumulate(nums.begin(),nums.end(),0LL);
+        int sum = accumulate(nums.begin(),nums.end(),0);
         if(sum%2 != 0) return false;
-        dp.assign(nums.size()+1,vector<ll>(2*sum+1,-1));
         target = sum / 2;
+        dp.assign(nums.size()+1,vector<int>(target+1,-1));
         return f(nums,0,0);
     }
 };
