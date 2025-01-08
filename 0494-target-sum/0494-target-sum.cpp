3b37dbc9e3c46122d32e127b55426fee4ddeaed4
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-01-09 00:35:17 +0530

    Time: 7 ms (77.56%), Space: 15.8 MB (40.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,24 +1,28 @@
-using ll = long long ;
-#include<bits/stdc++.h>
-
 class Solution {
 public:
-    ll maintarget;
-    vector<vector<ll>> dp;
-    ll offset = 0;
-    ll f(auto &nums,ll i , ll sum) {
-        if( i == nums.size() ) {
-            if(sum == maintarget) return 1;
-            else return 0 ;
-        }
-        if(dp[i][sum+offset] != -1 ) return dp[i][sum+offset];
-        return dp[i][sum+offset] = f(nums,i+1,sum+nums[i]) + f(nums,i+1,sum-nums[i]);
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        maintarget = target ;
-        ll sum = accumulate(nums.begin(),nums.end(),0LL);
-        for(int i =0 ; i < nums.size(); i++ ) offset += abs(nums[i]) ; 
-        dp.assign(nums.size()+1,vector<ll>(10000,-1));
-        return f(nums,0,0);
+        int tsum = accumulate(nums.begin(),nums.end(),0);
+        int tofind = (tsum + target) / 2;
+
+        if( (tsum + target) % 2 != 0 || tofind < 0) return 0;
+        
+        vector<vector<int>> dp(nums.size(),vector<int> (tofind+1,-1));
+
+        auto f = [&](int i,int T,auto&& f){
+            if(i == -1) {
+                if(T == 0) return 1;
+                else return 0;
+            }
+
+            if(dp[i][T] != -1) return dp[i][T];
+
+            int include = 0;
+            if(T - nums[i] >= 0) include = f(i - 1, T - nums[i], f);
+            int exclude = f(i-1,T,f);
+
+            return dp[i][T] = include + exclude; 
+        };
+
+        return f(nums.size() - 1, tofind, f) ; 
     }
 };
