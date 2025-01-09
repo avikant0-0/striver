8d305b36ab991e565a3f4ed3637ecd1407308c67
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-01-09 12:31:55 +0530

    Time: 7 ms (77.59%), Space: 15.8 MB (40.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -6,31 +6,23 @@ public:
 
         if( (tsum + target) % 2 != 0 || tofind < 0) return 0;
         
-        // vector<vector<int>> dp(nums.size(),vector<int> (tofind+1,0));
-        vector<int> prev(tofind+1,0);
+        vector<vector<int>> dp(nums.size(),vector<int> (tofind+1,-1));
 
-        // for(int i = 0; i < nums.size(); i++) dp[i][0] = 1;
-
-        if(nums[0] == 0) prev[0] = 2;
-        else prev[0] = 1 ;
-
-        if(nums[0] != 0 && nums[0] <= tofind) prev[nums[0]] = 1; 
-
-        for(int i = 1; i < nums.size(); i++)
-        {   
-            vector<int> curr(tofind + 1,0);
-            for(int T = 0; T <= tofind; T++)
-            {
-                int exclude = prev[T];
-
-                int include = 0 ;
-                if(T - nums[i] >= 0) include = prev[T-nums[i]];
-
-                curr[T] = exclude + include ;
+        auto f = [&](int i,int T,auto&& f){
+            if(i == -1) {
+                if(T == 0) return 1;
+                else return 0;
             }
-            prev = curr;
-        }
 
-        return prev[tofind] ; 
+            if(dp[i][T] != -1) return dp[i][T];
+
+            int include = 0;
+            if(T - nums[i] >= 0) include = f(i - 1, T - nums[i], f);
+            int exclude = f(i-1,T,f);
+
+            return dp[i][T] = include + exclude; 
+        };
+
+        return f(nums.size() - 1, tofind, f) ; 
     }
 };
