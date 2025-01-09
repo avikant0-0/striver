99e971ec22545548f255280fa8b530759387b79a
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-01-09 14:37:43 +0530

    Time: 45 ms (15.77%), Space: 27.5 MB (20.88%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string s, string t) {
-    int m = s.length() ;
-	int n  = t.length() ;
-	vector<vector<int>> dp ;
-	dp.assign(m+1 , vector<int> ( n +1 , -1 ));
-	for(int i = 0 ; i < m + 1 ; i++) dp[i][0] = 0 ;
-	for(int i = 0 ; i < n  + 1 ; i++) dp[0][i] = 0;
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
 
-	for(int i = 1; i < m + 1; i++){
-		for(int j = 1; j < n +1 ; j++){
-			if(s[i-1] == t[j-1]) dp[i][j] = 1 + dp[i-1][j-1] ;
-			else dp[i][j] = max(dp[i-1][j] , dp[i][j-1]) ;
-		}
-	}
-	return dp[m][n] ;
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+
+        auto f = [&](int i,int j,auto&& f)->int{
+            if(i < 0 || j < 0) return 0;
+
+            if(dp[i][j] != -1) return dp[i][j]; 
+
+            if(text1[i] == text2[j]) return dp[i][j] =  1 + f(i-1,j-1,f);
+            else return dp[i][j] = max(f(i,j-1,f),f(i-1,j,f));
+        };
+
+        return f(n-1,m-1,f);
     }
 };
