68d78e9d0341b8edbdddbf11d8986e8d1a0f7bf9
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-02-09 12:02:53 +0530

    Time: 3 ms (19.76%), Space: 16.5 MB (46.06%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,30 +11,17 @@
  */
 class Solution {
 public:
-    TreeNode* dfs(int preStart,int preEnd,int inStart,int inEnd,auto& preorder,auto& inorder,auto& mp){
-        if( (preStart>preEnd) || (inStart>inEnd)) return nullptr;
+    TreeNode* dfs(int& i,int ub,vector<int>& preorder){
+        if(i == preorder.size() || preorder[i] > ub) return nullptr;
+        TreeNode* root = new TreeNode(preorder[i++]);
 
-        int curr_root = preorder[preStart];
-        TreeNode* root = new TreeNode(curr_root);
-        int in_ind = mp[curr_root];
-
-        int num_left = in_ind - inStart; 
-
-        root->left = dfs(preStart+1,preStart+num_left,inStart,in_ind-1,preorder,inorder,mp);
-        root->right = dfs(preStart+num_left+1,preEnd,in_ind+1,inEnd,preorder,inorder,mp);
+            root->left = dfs(i,preorder[i-1],preorder);
+            root->right =dfs(i,ub,preorder);
 
         return root; 
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        vector<int> inorder = preorder;
-        sort(inorder.begin(),inorder.end());
-
-        map<int,int> mp;
-        int n = preorder.size();
-
-        for(int i = 0; i < n;i++) mp[inorder[i]] = i;
-
-        TreeNode* root;
-        return dfs(0,n-1,0,n-1,preorder,inorder,mp); 
+        int i = 0;
+        return dfs(i,INT_MAX,preorder);
     }
 };
