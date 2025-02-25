e148f733bd8ab4739acc7bc7935111f99a07ffb8
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-02-25 13:15:48 +0530

    Time: 7 ms (11.95%), Space: 17.5 MB (6.21%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void dfs(const vector<int>& nums, set<vector<int>>& st, int i, vector<int> res){
+        if(i == nums.size()){
+            sort(res.begin(),res.end());
+            st.insert(res);
+            return;
+        }
+        
+        res.push_back(nums[i]);
+        dfs(nums,st,i+1,res);
+        res.pop_back();
+        dfs(nums,st,i+1,res); 
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>> st; 
+        dfs(nums,st,0,vector<int>());
+        vector<vector<int>> answer(st.begin(),st.end());
+
+        return answer;  
+    }
+};
