094fa0f2cef10a6eaa5e9ba2b6c34cd6174d1cb6
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-02-27 14:12:42 +0530

    Time: 17 ms (24.73%), Space: 23.2 MB (19.66%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void dfs(int i,vector<int> tempans, int target, vector<int>&a, set<vector<int>>& st){
+        
+        if(target == 0) st.insert(tempans);
+        if(i >= a.size()) return; 
+        
+        if(target >= a[i] && i < a.size()){
+            tempans.push_back(a[i]);
+            dfs(i,tempans,target-a[i],a,st);
+            tempans.pop_back();
+            dfs(i+1,tempans,target,a,st); 
+        }else return; 
+    }
+    vector<vector<int>> combinationSum(vector<int>& a, int target) {
+        set<vector<int>> st;
+        int n = a.size();
+        sort(a.begin(),a.end());
+
+        dfs(0,vector<int>(),target,a,st);
+
+        vector<vector<int>> answer(st.begin(),st.end());
+        return answer; 
+    }
+};
