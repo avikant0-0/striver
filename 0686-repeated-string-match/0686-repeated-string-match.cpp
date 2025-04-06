33657c139559e5dc955ae2181a47c14927da53a9
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-04-06 16:48:02 +0530

    Time: 56 ms (17.43%), Space: 154.7 MB (6.9%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
-    int f(string a,string b){
-
-        long long mod = 1e9+7;
-
-        long long n = a.size(), m = b.size();
-        if(n < m) return false;
-
-        long long power = 1;
-        for(long long i = 0; i < m - 1; i++) power = (power * 10) % mod;
-        long long hash_b = 0,hash_a = 0;
-        for(long long i = 0; i < m; i++) hash_b = ((10*hash_b) % mod + (b[i]-'a'+1) % mod) % mod;
-
-        for(long long i = 0 ; i < n; i++){
-            if(i < m - 1) {
-                hash_a = ((10*hash_a) % mod + (a[i] - 'a' + 1) % mod) % mod;
-                continue;
-            }
-
-            hash_a = ((10*hash_a) % mod + (a[i] - 'a' + 1) % mod) % mod;
-            if(hash_a == hash_b) return true;
- 
-            hash_a = ((hash_a) % mod - (power * (a[i-m+1] - 'a' + 1) + mod) % mod) % mod;
-
-        }
-
-        return false;
-    }
     int repeatedStringMatch(string a, string b) {
-        int n = a.size(),m = b.size();
-        int r = m/n + (m%n!=0);
+        int n = ( b.length() + a.length() - 1 )/ a.length();
+        cout << n << endl;
+        string c = "";
+        for(int i = 0; i < n  ; i++) c = c + a; 
 
-        string t = a;
-        for(int i = 0; i <= r+2; i++){
-            if(f(a,b)) return i+1;
-            a += t;
+        cout << c << endl; 
+
+        for(int i =0; i < 2; i++){
+            if(c.find(b)!=string::npos){
+                return n + i ; 
+            }
+            c = c + a;
         }
-        
         return -1; 
     }
 };
