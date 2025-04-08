6bb9a8cf762a9eb374faf11d5645ea95cad19028
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-04-08 17:37:52 +0530

    Time: 6 ms (2.31%), Space: 9.2 MB (13.47%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,10 +1,48 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        if(haystack.size() < needle.size()) return -1;
-        for(int i = 0; i <= haystack.size() - needle.size(); i++){
-            if(haystack.substr(i,needle.size()) == needle) return i;
+    vector<int> Z_func(string pattern,string text){
+        string pattern_Text = pattern + '#' + text;
+
+        int t_len = pattern_Text.size();
+        int l = 0, r = 0;
+        vector<int> Z(t_len);
+        for(int k = 1; k < t_len; k++){
+            if(k > r){
+                int match = 0;
+                while(match + k < t_len && pattern_Text[match] == pattern_Text[match+k])
+                ++match;
+
+                Z[k] = match;
+                if(match){
+                    l = k; 
+                    r = k + match - 1;
+                }
+            }else{
+                int p = k - l;
+                if(Z[p] < r - k + 1){
+                    Z[k] = Z[p];
+                }else{
+                    int i = r + 1;
+                    while(i < t_len && pattern_Text[i] == pattern_Text[i-k]) ++i;
+
+                    Z[k] = i - k;
+                    l = k;
+                    r = i - 1;
+                }
+            }
         }
+
+        return Z;
+    }
+    int strStr(string haystack, string needle) {
+        vector<int> Z = Z_func(needle,haystack);
+
+        for(int i = 0; i < Z.size(); i++) {
+            if(Z[i] == needle.size()) return i - needle.size()-1;
+        }
+
+        for(int i = 0; i < Z.size(); i++) cout << Z[i] <<" " ; 
+
         return -1;
     }
 };
