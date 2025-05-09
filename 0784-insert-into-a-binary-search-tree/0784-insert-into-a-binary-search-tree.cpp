50ed6517a777163b7e0023e8a9fb5e6abe7fc090
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-09 23:13:00 +0530

    Time: 0 ms (100%), Space: 59.4 MB (44.11%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
@@ -11,35 +11,16 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root,auto& arr){
-        if(!root) return;
-        arr.push_back(root);
-        dfs(root->left,arr);
-        dfs(root->right,arr);
-    }
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        
-        vector<TreeNode*> nodes;
-        dfs(root,nodes);
-        TreeNode* valnode = new TreeNode(val);
-        nodes.push_back(valnode);
-        sort(nodes.begin(),nodes.end(),[&](auto& a,auto& b){
-            return a->val < b->val;
-        });
-        if(nodes.size() == 0) return nullptr;
-        if(nodes.size() == 1){
-            return nodes[0];
+        if(!root) return new TreeNode(val);
+        TreeNode *node = root, *prev;
+        while(node != NULL){
+            prev = node;
+            if(val > node->val) node = node->right;
+            else node = node->left;
         }
-        root = nodes[0];
-
-        for(int i = 1; i < nodes.size(); i++){
-            root->right = nodes[i];
-            root->left = nullptr;
-            root = root->right;
-        }
-        nodes[nodes.size()-1]->right = nullptr;
-        nodes[nodes.size()-1]->left = nullptr;
-
-        return nodes[0];
+        if(val > prev->val) prev -> right = new TreeNode(val);
+        else prev -> left = new TreeNode(val);
+        return root;
     }
 };
