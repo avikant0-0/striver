787745295171130b50df46c220fe8ca9c6a1392b
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-10 21:49:22 +0530

    Time: 0 ms (100%), Space: 34.2 MB (93.66%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int val) {
+        TreeNode *node = root,*prev = nullptr;
+        while(node){
+          
+            if(node->val<val) prev = node, node = node->right;
+            else if(node->val>val) prev = node, node = node->left;
+            else break;
+          
+
+        }
+        if(!node) return root;
+
+        if(!prev){
+            if(!node->left && !node->right){
+                return nullptr;
+            }
+            if(!node->left || !node->right){
+                if(node->left) return root->left;
+                else return root->right;
+            }
+            TreeNode* to_attach = node->left;
+            TreeNode* left_most = node->right;
+
+            while(left_most->left != nullptr) left_most = left_most->left;
+
+            left_most -> left = to_attach;
+            return root->right;
+        }
+
+        
+        if(!node->left && !node->right){
+            if(prev->val > node->val) {
+                prev->left = nullptr;
+                return root;
+            }else{
+                prev->right = nullptr;
+                return root;
+            }
+        }
+
+        if(!node->left || !node->right){
+            if(prev->val > node->val) {
+                prev->left = (node->left? node->left: node->right);
+                return root;
+            }else{
+                prev->right = (node->left? node->left: node->right);
+                return root;
+            }
+        }
+
+        TreeNode* to_attach = node->left;
+        if(prev->val > node->val) {
+            prev->left = node->right;
+        }else{
+            prev->right = node->right;
+        }
+
+        TreeNode* left_most = node->right;
+        while(left_most->left != nullptr) left_most = left_most->left;
+
+        left_most -> left = to_attach;
+
+        return root;
+    }
+};
