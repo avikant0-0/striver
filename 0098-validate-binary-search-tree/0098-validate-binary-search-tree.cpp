77b3bdd32ae17e78759b8dee6ef1dc262fb574bd
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-11 14:51:51 +0530

    Time: 0 ms (100%), Space: 21.8 MB (94.89%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,17 +11,15 @@
  */
 class Solution {
 public:
-    bool dfs(TreeNode* root, long long lb, long long ub) {
-        if (!root) return true; 
-        if (root->val > ub || root->val < lb) return false;
-        
-        bool left = dfs(root->left, lb, static_cast<long long>(root->val) - 1);
-        bool right = dfs(root->right, static_cast<long long>(root->val) + 1, ub);
-        
-        return left && right; 
+    bool dfs(TreeNode* root,long long mn,long long mx){
+        if(!root) return true;
+        if(root->val >= mx || root->val <= mn) return false;
+        bool left = dfs(root->left,mn,min(mx,(long long)root->val));
+        bool right = dfs(root->right,max(mn,(long long)root->val),mx);
+
+        return left && right;
     }
-    
     bool isValidBST(TreeNode* root) {
-        return dfs(root, INT_MIN, INT_MAX);
+        return dfs(root,-1e10,1e10);
     }
 };
