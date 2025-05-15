6dd133b792381c81cbf698b43c36958eab54b52a
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-15 13:50:27 +0530

    Time: 32 ms (5.28%), Space: 73 MB (5.44%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,12 +1,32 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& a) {
-        int sum = 0,mx = INT_MIN;
-        for(int i = 0; i < a.size(); i++){
-            sum += a[i];
-            mx = max(mx,sum);
-            if(sum < 0) sum = 0;
+
+    int find_cross(int i,int mid,int j,vector<int>& a){
+        int left = 0;
+        int left_max = INT_MIN;
+        for(int it = mid; it >= i; it--){
+            left += a[it];
+            left_max = max(left_max,left);
         }
-        return mx;
+        int right = 0;
+        int right_max = INT_MIN;
+        for(int it = mid+1; it <= j; it++){
+            right += a[it];
+            right_max = max(right_max,right);
+        }
+        return left_max + right_max;
+    }
+    int dfs(int i,int j,vector<int>& a){
+        if(i == j) return a[i];
+        int mid = (i+j)/2;
+        int left = dfs(i,mid,a);
+        int right = dfs(mid+1,j,a);
+
+        int cross = find_cross(i,mid,j,a);
+
+        return max({cross,left,right});
+    }
+    int maxSubArray(vector<int>& a) {
+        return dfs(0,a.size()-1,a);
     }
 };
