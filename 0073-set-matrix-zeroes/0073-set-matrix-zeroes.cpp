4673facc8beb73ded5e5f6470d84c372974a7770
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-16 09:51:17 +0530

    Time: 16 ms (3.4%), Space: 16.5 MB (100%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,32 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& a) {
-        int n = a.size(), m = a[0].size();
-        int col = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(j == 0){
-                    if(a[i][j] == 0) col = -159,a[i][0] = -159;
+        ll n = a.size() ;
+        ll m = a[0].size(); 
+        ll col1 = 1;
+        ll row1 = 1;  
+        for(ll i= 0 ; i < n ;i++ ) {
+            for(ll j = 0 ; j < m ;j++ ) {
+                if(a[i][j] == 0 ) {
+                    a[0][j] = 0 ;
+                    a[i][0] = 0 ;
+                    if(i == 0  ) col1 = 0 ;
+                    if(j == 0  ) row1 = 0 ; 
                 }
-                else if(a[i][j] == 0) a[0][j] = -159, a[i][0] = -159;
             }
         }
-        for(int i = 1; i < n; i++){
-            if(a[i][0] == -159){
-                for(int j = 0; j < m; j++) a[i][j] = 0;
+        for(ll i =0  ;i < n; i++ ) {
+            if(i == 0 ) continue; 
+            if(a[i][0] == 0 ) {
+                for(ll j= 0  ; j < m ; j++ ) a[i][j] = 0 ; 
             }
         }
-        for(int j = 1; j < m; j++){
-            if(a[0][j] == -159){
-                for(int i = 0; i < n;i++) a[i][j] = 0;
+        for(ll i =0  ;i < m; i++ ) {
+            if(i == 0 ) continue;
+            if(a[0][i] == 0 ) {
+                for(ll j= 0  ; j < n ; j++ ) a[j][i] = 0 ; 
             }
         }
-        if(a[0][0] == -159){
-            for(int j = 0; j < m; j++) a[0][j] = 0;
+        if(col1 == 0 ) {
+            for(ll i =0  ; i< m ;i++ ) a[0][i] = 0 ; 
+           
         }
-        // cout << col << endl;
-        if(col == -159){
-            for(int i = 0; i < n; i++) a[i][0] = 0;
+        if(row1 == 0 ) {
+             for(ll i =0  ; i< n ;i++ ) a[i][0] = 0 ; 
         }
     }
 };
