95f1a60d9ee3158d749da368109255ccc071810f
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-20 22:24:04 +0530

    Time: 0 ms (100%), Space: 8.6 MB (50.89%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long l = 0, r = 1e9;
+        if(!x) return x;
+        int l = 1, r = 1e9;
         while(l <= r){
-            long long  mid = r - (r-l) / 2 ;
-            if(mid * mid < x) l = mid + 1;
+            int  mid = r - (r-l) / 2 ;
+            if(mid <= x / mid) l = mid + 1;
             else r = mid - 1;
         }
-        if((r+1)*(r+1) == x) return (r+1);
-        else return r;
+        return r;
     }
 };
