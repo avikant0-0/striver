756d780b96c0e8d4830bcb32f22a5becbe970e2d
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-23 15:18:06 +0530

    Time: 45 ms (40.82%), Space: 79.1 MB (8.29%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     int totalFruit(vector<int>& a) {
         int n = a.size();
-        set<int> type;
         int i = 0, j = 0;
         int ans = 0;
         map<int,int> mp;
 
         while(j < n){
-            type.insert(a[j]);
             mp[a[j]]++;
 
-            if(type.size() <= 2){
+            if(mp.size() <= 2){
                 ans = max(ans,j-i+1);
                 ++j;
             }
             else{
-                while(type.size() > 2){
+                while(mp.size() > 2){
                     mp[a[i]]--;
-                    if(mp[a[i]] == 0) type.erase(a[i]);
+                    if(mp[a[i]] == 0) mp.erase(a[i]);
                     ++i;
                 }
                 ans = max(ans,j-i+1);
