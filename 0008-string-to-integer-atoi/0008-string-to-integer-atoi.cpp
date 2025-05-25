924aa018dd0f1be4ee467b170592de7d845c3b4b
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-25 12:21:33 +0530

    Time: 0 ms (100%), Space: 9.1 MB (56.77%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -3,24 +3,24 @@ public:
     int myAtoi(string s) {
         int n = s.size();
         int i = 0;
-        while(i < n && s[i] == ' ') ++i;
-        int sign = 1;
+        while(s[i] == ' ') ++i;
 
-        if(s[i] == '+') sign = 1, ++i;
-        else if(s[i] == '-') sign = -1, ++i;
+        int sign;
+        if(s[i] == '-') sign = -1,++i;
+        else if(s[i] == '+') sign = 1,++i;
+        else sign = 1;
 
-        long res = 0;
-
-        while(i < n){
-            if(s[i] > '9' || s[i] < '0') break;
-            if(res > INT_MAX/10) return sign == 1 ? INT_MAX : INT_MIN;
-            res = res * 10 + (s[i] - '0');
+        long long ans = 0;
+        while( s[i] <= '9' && s[i] >= '0'){
+            ans = ans*10 + (s[i]-'0');
+            if(ans >= INT_MAX) break;
             ++i;
         }
 
+        ans *= sign;
 
-        if(res*sign >= INT_MAX) return INT_MAX;
-        else if(res*sign <= INT_MIN) return INT_MIN; 
-        return res*sign;
+        if(ans >= INT_MAX) return INT_MAX;
+        else if(ans <= INT_MIN) return INT_MIN;
+        else return int(ans);
     }
 };
