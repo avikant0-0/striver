8cd2219cc33408e5671b646f789d3aff0cb0e829
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-25 21:20:02 +0530

    Time: 0 ms (100%), Space: 12.9 MB (89.12%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void f(int open,int close,int n,auto& temp,auto& ans){
+        if(close > open || open > n || close > n) return;
+
+        if(open == n && close == n){
+            ans.push_back(temp);
+            return;
+        }
+
+        if(open == close){
+            temp.push_back('(');
+            f(open+1,close,n,temp,ans);
+            temp.pop_back();
+        }
+        else{
+            temp.push_back('(');
+            f(open+1,close,n,temp,ans);
+            temp.pop_back();
+
+            temp.push_back(')');
+            f(open,close+1,n,temp,ans);
+            temp.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string temp;
+        f(0,0,n,temp,ans);
+        return ans;
+    }
+};
