21f39ad596f4361cc1ed2db16e0f29b1c384d50b
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-25 13:42:23 +0530

    Time: 0 ms (100%), Space: 8.4 MB (91.96%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    using db = double;
+    double myPow(double x, int n) {
+        if(x == 0) return 0;
+        if(n == 0) return 1;
+
+        long long N = n;
+        if(N < 0){
+             x = 1/x;
+             N = -N;
+        }
+        db ans = 1;
+        while( N > 0 ){
+            if(N%2 == 1){
+               ans *= x;
+               N--;
+            }
+            else{
+                x *= x;
+                N /= 2;
+            }
+        }
+        return ans;
+    
+    }
+};
