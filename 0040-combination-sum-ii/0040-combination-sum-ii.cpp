a8db5418f693a01620e091ed2416bd5448fc0469
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-26 13:56:13 +0530

    Time: 3 ms (41.84%), Space: 14 MB (61.05%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    void dfs(int ind,int target,auto& A,auto& r,auto t){
-        int n = A.size();
-        if(ind == -1 ){
-            if(target == 0) r.push_back(t);
+    void f(int ind,int sum,auto& a,auto& ans,auto& temp,int t){
+        if(sum > t){
             return;
         }
-
-        //take
-        if(A[ind]<=target){
-            t.push_back(A[ind]);
-            dfs(ind-1,target - A[ind],A,r,t);
-            t.pop_back();
+        if(ind >= a.size()){
+            if(sum == t) ans.push_back(temp);
+            return;
         }
-        
-        //nottake
-        int lb = lower_bound(A.begin(),A.end(),A[ind])-A.begin();
-        dfs(lb-1,target,A,r,t);
+        temp.push_back(a[ind]);
+        f(ind+1,sum+a[ind],a,ans,temp,t);
+        temp.pop_back();
 
+        int ind2 = upper_bound(a.begin(),a.end(),a[ind]) - a.begin();
+        f(ind2,sum,a,ans,temp,t);
     }
-    vector<vector<int>> combinationSum2(vector<int>& A, int T) {
-        sort(A.begin(),A.end());
-        vector<vector<int>> r; 
-        dfs(A.size()-1,T,A,r,vector<int>());
+    vector<vector<int>> combinationSum2(vector<int>& a, int t) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(a.begin(),a.end());
+        f(0,0,a,ans,temp,t);
 
-        return r;
+        return ans;
     }
 };
