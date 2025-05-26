5f6832f0282b550d373d5c22056675c90c2f74ff
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-26 22:23:42 +0530

    Time: 227 ms (75.31%), Space: 10.7 MB (63.12%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> visited;
-    bool f(int i,int j,int len,auto& temp,auto& a,auto& w){
+    bool f(int i,int j,int len,auto& a,auto& w){
         int n = a.size(), m = a[0].size();
 
         if(len == w.size()){
-            if(temp == w) return true; 
-            return false;
+            return true; 
         }
         if(i < 0 || i >= n) return false;
         if(j < 0 || j >= m) return false;
@@ -21,23 +20,17 @@ public:
         for(int ind = 0; ind <= 3; ind++){
             int n_i = i+dx[ind], n_j = j+dy[ind];
 
-            // if(n_i < 0 || n_i >= n) continue;
-            // if(n_j < 0 || n_j >= m) continue;
-
-            temp.push_back(a[i][j]);
-            if( f(n_i,n_j,len+1,temp,a,w) ) return true;
-            temp.pop_back();
+            if( f(n_i,n_j,len+1,a,w) ) return true;
         }
         visited[i][j] = 0;
         return false;
     }
     bool exist(vector<vector<char>>& a, string w) {
-        string temp;
         for(int i = 0; i < a.size(); i++){
             for(int j = 0; j < a[0].size(); j++){
                 visited.assign(a.size(),vector<int>(a[0].size(),0));
 
-                if( f(i,j,0,temp,a,w) ){
+                if( f(i,j,0,a,w) ){
                     return true;
                 }
             }
