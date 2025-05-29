bb1b3100a1249ce5b183d800dbfd060cebeea9c3
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-29 12:23:21 +0530

    Time: 0 ms (100%), Space: 8.1 MB (83.4%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -2,18 +2,10 @@ class Solution {
 public:
     bool rotateString(string s, string g) {
         if(s.size() != g.size()) return false;
-        for(int k = 0; k < s.size(); k++){
-            if(s[k] != g[0]) continue;
-            int i = k;
-            for(int j = 0; j < s.size(); j++){
-                if(s[i] == g[j]){
-                    if(j == s.size() - 1){
-                        return true;
-                    }
-                    i = ( i + 1 ) % s.size();
-                    continue;
-                }else break;
-            }
+
+        for(int i = 1; i <= s.size(); i++){
+            rotate(s.begin(),s.begin()+1,s.end());
+            if(s == g) return true;
         }
 
         return false;
