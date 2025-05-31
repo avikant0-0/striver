52f2d649cb4d454fcd4578bc8a7e6594487aed67
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-31 15:04:55 +0530

    Time: 0 ms (100%), Space: 21.5 MB (25.58%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<int> dp;
-    int f(int i,auto& a){
-        if(i >= a.size() - 1) return 0;
-
-        if(dp[i] != -1) return dp[i];
-
-        int ans = 1e9;
-        for(int j = 1; j <= a[i]; j++){
-            ans = min(ans,1 + f(i+j,a));
-        }
-        return dp[i] = ans;
-    }
     int jump(vector<int>& a) {
-        dp.assign(a.size(),-1);
-        return f(0,a);
+        int n = a.size();
+        if(n == 1) return 0;
+        set<pair<int,int>> st;
+        for(int i = 0; i < n; i++){
+            if(st.empty()){
+                st.insert({min(i+a[i],n-1),1});
+                continue;
+            }
+            if(a[i] + i <= st.rbegin()->first) continue;
+            
+            int t = st.lower_bound({i,0})->second;
+            st.insert({min(i+a[i],n-1),t+1});
+        }
+        return st.lower_bound({n-1,0})->second;
     }
 };
