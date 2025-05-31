0e2ceac2bef4e30ad14749f3016084939c33d165
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-05-31 14:00:20 +0530

    Time: 0 ms (100%), Space: 52.1 MB (87.27%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    vector<int> memo;
-    bool f(int i,auto& a){
-        if(i >= a.size() - 1) return true;
-        if(memo[i] != -1) return memo[i];
-
-        for(int jump = 1; jump <= a[i]; jump++){
-            if(f(i+jump,a)){
-                return memo[i] = true;
-            }
-        }
-        return memo[i] = false;
-    }
     bool canJump(vector<int>& a) {
-        int n = a.size();
-        memo.assign(n, -1);
-        return f(0,a);
+        int mx = 0;
+        for(int i = 0; i < a.size(); i++){
+            if(i > mx) return false;
+            mx = max(mx,a[i]+i);
+        }
+        return true;
     }
 };
