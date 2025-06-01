d62f532a5e0fbe3a9aa17799689ce80e54b705ea
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-06-01 20:14:55 +0530

    Time: 51 ms (58.81%), Space: 137.5 MB (36.22%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,36 +1,42 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<int> visited;
-    void dfs(int u, vector<vector<int>>& adj, int& v, int& e) {
-        visited[u] = 1;
-        ++v;
-        for (int w : adj[u]) {
-            ++e;
-            if (!visited[w]) {
-                dfs(w, adj, v, e);
+    bool bfs(int k,auto& adj){
+        queue<int> q;
+        q.push(k);
+        int v = 0, e = 0;
+        visited[k] = 1;
+
+        while(!q.empty()){
+            int top = q.front();
+            q.pop();
+            ++v;
+
+            for(int i : adj[top]){
+                ++e;
+
+                if(!visited[i]){
+                    visited[i] = 1;
+                    q.push(i);
+                }
             }
         }
+        e/=2;
+        return v*(v-1)/2 == e;
     }
+    int countCompleteComponents(int n, vector<vector<int>>& e) {
+        visited.assign(n,0);
+        vector<vector<int>> adj(n,vector<int>());
 
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        visited.assign(n, 0);
-        vector<vector<int>> adj(n);
-        for (auto& edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+        for(int i = 0; i < e.size(); i++){
+            adj[e[i][0]].push_back(e[i][1]);
+            adj[e[i][1]].push_back(e[i][0]);
         }
+
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                int v = 0, e = 0;
-                dfs(i, adj, v, e);
-                e /= 2;
-                if (e == v * (v - 1) / 2) {
-                    ++ans;
-                }
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                if(bfs(i,adj))  ++ans;
             }
         }
         return ans;
