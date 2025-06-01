b498715ddcf41a3bcaccbe62d96df7138370b44f
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-06-01 19:36:57 +0530

    Time: 0 ms (100%), Space: 19.5 MB (72.47%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-
-    void BFS(int sourceNode,vector<vector<int>>&
-        adj, vector<int>& visited){
-
-        queue<int> q;
-        
-        q.push(sourceNode);
-        visited[sourceNode] = 1;
-        
-        while(!q.empty()){
-            int front = q.front();
-            q.pop(); 
-            
-            
-            for(int i = 0; i < adj[0].size() ; i++){
-                if(adj[front][i] == 1 && !visited[i]){
-                    visited[i] = 1; 
-                    q.push(i);
-                }
+    vector<int> visited;
+    void dfs(int k,auto& a){
+        if(visited[k]) return;
+        visited[k] = 1;
+        for(int i = 0; i < a.size(); i++){
+            if(a[k][i] == 1 && k != i){
+                dfs(i,a);
             }
-            
         }
     }
-
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int ans = 0;
-        int n =isConnected.size();
-        vector<int> visited(n,0);
-
-        for(int  i =0; i < n; i++) {
+    int findCircleNum(vector<vector<int>>& a) {
+        int n = a.size(), ans = 0;
+        visited.assign(n,0);
+        for(int i =0 ; i < n; i++){
             if(!visited[i]){
+                dfs(i,a);
                 ++ans;
-                BFS(i,isConnected,visited);
             }
         }
         return ans;
