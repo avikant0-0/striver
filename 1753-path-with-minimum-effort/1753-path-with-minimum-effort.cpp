bf9fe48f3de40f1a14b6af91ae8d959f9e59251f
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-06-05 19:53:24 +0530

    Time: 102 ms (27.52%), Space: 22.4 MB (98.25%) - LeetHub

---
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& a) {
+        int n = a.size(), m = a[0].size();
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+
+        priority_queue<tuple<int,int,int>,
+                    vector<tuple<int,int,int>>,
+                    greater<tuple<int,int,int>>> q;
+
+        q.push(make_tuple(0,0,0));
+
+        dist[0][0] = 0;
+        int dx[] = {0,0,1,-1};
+        int dy[] = {1,-1,0,0};
+
+        while(!q.empty())
+        {
+            auto [oi,oj,mx] = q.top();
+            q.pop();
+
+            for(int i = 0; i < 4; i++)
+            {
+                int ni = oi + dx[i], nj = oj + dy[i];
+                if(ni < 0 || ni >= n || nj < 0 || nj >= m) continue;
+
+                int newstep = max(mx,abs(a[ni][nj] - a[oi][oj]));
+                if(newstep < dist[ni][nj])
+                {
+                    dist[ni][nj] = newstep;
+                    q.push(make_tuple(ni,nj,newstep));
+                }
+            }
+        }
+
+        return dist[n-1][m-1];
+    }
+};
