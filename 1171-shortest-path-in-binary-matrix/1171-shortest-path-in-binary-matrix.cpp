82a42b2f03bbd7a6f185d651c32edbc60b1b9d29
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-06-05 19:32:37 +0530

    Time: 10 ms (91.72%), Space: 26.4 MB (34.68%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if(grid[0][0] == 1) return -1; 
-
-
-        int n = grid.size();
-        int ans = INT_MAX; 
-        queue<pair<pair<int,int>,int>> q;
-        q.push(make_pair(make_pair(0,0),1));
+    int shortestPathBinaryMatrix(vector<vector<int>>& a) {
+        int n = a.size();
+        queue<tuple<int,int,int>> q;
 
         vector<vector<int>> visited(n,vector<int>(n,0));
-        visited[0][0] = true; 
+        int ans = INT_MAX;
+        if(a[0][0] == 0)
+        {
+            q.push(make_tuple(0,0,1));
+            visited[0][0] = 1;
+        } 
 
-        while(!q.empty()){
-            auto [x,y] = q.front().first;
-            int curr_dist = 	q.front().second; 
-            if(x == n- 1 && y == n -1 ) ans = min(ans,curr_dist); 
+        while(!q.empty())
+        {
+            auto [oi,oj,steps] =  q.front();
             q.pop();
-            
-            for(int i = -1; i <= 1; i++){
-                for(int j = -1; j <= 1; j++){
-                    if(x + i >= 0 && y + j >= 0 && x + i < n && y + j < n 
-                    && !visited[x+i][y+j]  && grid[x+i][y+j] == 0){
-                        visited[x+i][y+j] = 1; 
-                        q.push(make_pair(make_pair(x+i,y+j),curr_dist+1));
-                    }
+            if(oi == n - 1 && oj == n - 1) ans = min(ans,steps);
+
+            for(int i = -1; i <= 1; i++)
+            {
+                for(int j = -1; j <= 1; j++)
+                {   
+                    int ni = oi + i, nj = oj + j;
+                    if(ni < 0 || ni >= n || nj < 0 || nj >= n) continue;
+                    if(a[ni][nj] == 1) continue;
+                    if(visited[ni][nj]) continue;
+    
+                    visited[ni][nj] = 1;
+                    q.push(make_tuple(ni,nj,1+steps));
                 }
             }
-
-            
         }
-        if(visited[n-1][n-1] == 1) return ans;
-		else return -1; 
+
+        if(ans == INT_MAX) return -1;
+        else return ans;
     }
 };
