17d535ee76531f1d83bd55b9e7c627b3eff3ba90
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-06-07 23:18:54 +0530

    Time: 0 ms (100%), Space: 8.8 MB (37.81%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int divide(long long dividend, long long divisor) {
-        if(dividend == divisor) return 1;
-        if(dividend == -divisor) return -1;
         int sign = 1;
         if(dividend < 0) sign = sign * -1;
         if(divisor  < 0) sign = sign * -1;
-        long long ans = 0;
+        long  ans = 0;
 
         dividend = abs(dividend);
         divisor = abs(divisor);
@@ -14,11 +12,15 @@ public:
         while(dividend >= divisor)
         {   
             int i = 0;
-            while((divisor << (i+1)) < dividend) ++i;
+            while((divisor<<(i+1)) < dividend) 
+            {
+                // cout << (divisor<<(i+1)) << " " << i  << endl;
+                ++i;
+            }
 
 
-            dividend -= (divisor << i);
-            ans = ans + (1<<(i));
+            dividend -= divisor << (i);
+            ans += 1<<i;
 
             if(ans * sign <= INT_MIN) return INT_MIN;
             if(ans * sign >= INT_MAX) return INT_MAX;
