7f559dc9c129476836308dbfe902566704d63098
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-06-09 14:37:59 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    vector<int> dp;
-    int f(int i,vector<int>& a)
-    {
-        int n = a.size();
-        if(i >= n ) return 0;
-
-        if(dp[i] != -1) return dp[i];
-
-        int curr = f(i+2,a) + a[i];
-        int adj = f(i+1,a);
-
-        return dp[i] = max(curr,adj);
-    }
     int rob(vector<int>& a) {
-        dp.assign(a.size(),-1);
-        return f(0,a);
+        int n = a.size();
+        if(n == 1) return a[0];
+        vector<int> dp(a.size(),0);
+        dp[0] = a[0];
+        dp[1] = max(a[0],a[1]);
+
+        for(int i = 2; i < a.size(); i++)
+        {
+            dp[i] = max(dp[i-2] + a[i] , dp[i-1]);
+        }
+        return dp[a.size()-1];
     }
 };
