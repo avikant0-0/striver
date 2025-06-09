7902dc69cfbfed4795cf77b0c1acd56ce0adcd53
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-06-09 21:54:09 +0530

    Time: 91 ms (8.11%), Space: 20.9 MB (73.32%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
-    int f(int i,int j1,int j2,auto& a)
-    {
-        int n = a.size(), m = a[0].size();
-        
-        if(j1 < 0 || j2 < 0 || j1 >= m || j2 >= m)
-            return INT_MIN;
-        
-        if(i >= n)
-        {
-            return 0;
-        }
 
-        if(dp[i][j1][j2] != INT_MIN) return dp[i][j1][j2];
-    
-        int ans = INT_MIN;
-        
-        for(int dj1 = -1; dj1 <= 1; dj1++)
-        {
-            for(int dj2 = -1; dj2 <= 1; dj2++)
-            {
-                int nj1 = j1 + dj1, nj2 = j2 + dj2;
-                ans = max(ans,f(i+1,nj1,nj2,a));
-            }
-        }
-        return dp[i][j1][j2] = ans + (j1 == j2 ? a[i][j1] : a[i][j1] + a[i][j2]);
-    }
     int cherryPickup(vector<vector<int>>& a) {
         int n = a.size(), m = a[0].size();
-        dp.assign(n,vector<vector<int>>(m,vector<int>(m,INT_MIN)));
-        return f(0,0,m-1,a);
+        vector<vector<int>> up(m,vector<int>(m,INT_MIN));
+
+        up[0][m-1] = (m-1 != 0 ? a[0][0] + a[0][m-1] : a[0][0]);
+
+        for(int i = 1; i < n; i++)
+        {   
+            vector<vector<int>> temp(m,vector<int>(m,INT_MIN));
+            for(int j1 = 0; j1 < m; j1++)
+            {
+                for(int j2 = 0; j2 < m; j2++)
+                {
+                    for(int dj1 = -1; dj1 <= 1; dj1++)
+                    {
+                        for(int dj2 = -1; dj2 <= 1; dj2++)
+                        {
+                            int nj1 = j1 + dj1, nj2 = j2 + dj2;
+                            if(nj1 < 0 || nj2 < 0 || nj1 >= m || nj2 >= m) continue;
+                            temp[j1][j2] = max(temp[j1][j2],up[nj1][nj2]+ (j1 == j2 ? a[i][j1] : a[i][j1] + a[i][j2]));
+                        }
+                    }
+                }
+            }
+            up = temp;
+
+        }
+        int ans = INT_MIN;
+
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < m; j++)
+                ans = max(ans,up[i][j]);
+        }
+        return ans;
     }
+
+    // O(10 * n * m * m + m * m)
+    // O(m*m(1+10*n)) 
 };
