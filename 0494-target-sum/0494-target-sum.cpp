39a498999c3a9bec3997783ef4967cc0a9dde073
Avanikant Kumar Kashyap <105282352+avikant0-0@users.noreply.github.com>
2025-06-10 13:13:15 +0530

    Time: 3 ms (87.94%), Space: 15.2 MB (55.31%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
+    
+int findWays(vector<int>& a, int k)
+{	
+	int n = a.size();
+	vector<int> up(k+1,0);
+
+	up[0] = 1;
+	
+	
+	for(int i = 1; i <= n; i++)
+	{	
+		vector<int> temp(k+1,0);
+		for(int j = 0; j <= k; j++)
+		{
+			int nottake = up[j];
+			int take = 0;
+			if(a[i-1] <= j)
+				take = up[j-a[i-1]];
+			
+			temp[j] = (take + nottake);
+		}
+		up = temp;
+	}
+	return up[k];
+}
     int findTargetSumWays(vector<int>& nums, int target) {
         int tsum = accumulate(nums.begin(),nums.end(),0);
         int tofind = (tsum + target) / 2;
 
         if( (tsum + target) % 2 != 0 || tofind < 0) return 0;
         
-        vector<vector<int>> dp(nums.size(),vector<int> (tofind+1,-1));
-
-        auto f = [&](int i,int T,auto&& f){
-            if(i == -1) {
-                if(T == 0) return 1;
-                else return 0;
-            }
-
-            if(dp[i][T] != -1) return dp[i][T];
-
-            int include = 0;
-            if(T - nums[i] >= 0) include = f(i - 1, T - nums[i], f);
-            int exclude = f(i-1,T,f);
-
-            return dp[i][T] = include + exclude; 
-        };
-
-        return f(nums.size() - 1, tofind, f) ; 
+        return findWays(nums,tofind);
     }
 };
